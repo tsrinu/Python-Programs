d74aee9aa6154e45ea7c5a92ccc19632dc31c2e3
Srinivasarao T <tsrinu54@gmail.com>
2020-12-08 17:05:12 +0530

    Add files via upload

---
+++ checkGreater.py
@@ -0,0 +1,12 @@
+def checkGreater(number):
+    '''This function checks whether the entered number is greater than those in the list'''
+    original = [1,2,3,4,5]
+    original.sort()
+    if number > original[-1]:
+        print('Yes, the entered number is greater than those in the list')
+    else:
+        print('No, entered number is less than those in the list')
+
+if __name__ == '__main__':
+    userInput = int(input('Enter the number to check: '))
+    checkGreater(userInput)
